dc4d338231a3ab5e245a1bbd80bdf2d937fd3996
vchiragiev <vchiragiev@gmail.com>
2018-08-28 10:22:47 -0500

    commit

--- llist.py
+++ llist.py
@@ -1,19 +1,78 @@
-class Node(object):
 
-    def __init__(self, data=None, next_node=None):
+class Node:
+
+    # Constructor to initialize the node object
+    def __init__(self, data):
         self.data = data
-        self.next_node = next_node
+        self.next = None
 
-    def get_data(self):
-        return self.data
+    def detect_loop(self):
+        slow_p = fast_p = self
 
-    def get_next(self):
-        return self.next_node
+        while slow_p and fast_p and fast_p.next:
+            slow_p = slow_p.next
+            fast_p = fast_p.next.next
 
-    def set_next(self, new_next):
-        self.next_node = new_next
+            # If slow_p and fast_p meet at some point then there is a loop
+            if slow_p == fast_p:
+                # Return slow or fast to indicate that loop is found
+                return slow_p
 
-    def insert(self, data):
+                # Return 0 to indicate that there is no loop
+        return None
+
+        # Function to remove loop
+
+    # loop_node --> pointer to one of the loop nodes
+    def remove_loop(self, loop_node):
+        # Fix one pointer to head
+        # And the other pointer to k (# of nodes in the loop) nodes after head
+        ptr1 = loop_node
+        ptr2 = self
+        while ptr1.next != loop_node:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+        ptr1 = self
+
+        # Move both pointers at the same place
+        # they will meet at loop starting node
+        while ptr2 != ptr1:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        # Get pointer to the last node
+        ptr2 = ptr2.next
+        while ptr2.next != ptr1:
+            ptr2 = ptr2.next
+
+        # Set the next node of the loop ending node
+        # to fix the loop
+        ptr2.next = None
+
+    # Function to insert a new node at the beginning
+    def push(self, data):
         new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
+        new_node.next = self
+        return new_node
+
+    # Utility function to print the linked LinkedList
+    def print_list(self):
+        temp = self
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+
+if __name__ == "__main__":
+    tail = Node(0)
+    head = tail
+    for i in range(1,10):
+        head = head.push(i)
+
+    # Create a loop for testing
+    tail.next = head.next.next.next
+    loop_node = head.detect_loop()
+    print("Loop node: " + "none" if loop_node is None else loop_node.data)
+
+    print("Linked List after removing loop")
+    #head.print_list()
