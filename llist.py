ded6c22aad7283c61b9dc9d4ca2933dda5a01b39
vchiragiev <vchiragiev@gmail.com>
2018-08-28 08:08:26 -0500

    commit

---
+++ llist.py
@@ -0,0 +1,19 @@
+class Node(object):
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
