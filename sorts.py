b5bff927e28e933eafae3ef1c364a613f7030895
vchiragiev <vchiragiev@gmail.com>
2018-08-29 13:32:42 -0500

    commit

--- sorts.py
+++ sorts.py
@@ -14,9 +14,9 @@ def split(data, i_left: int, i_right: int):
     i_mid = i_left
     i_left += 1
     while i_right >= i_left:
-        while i_left <= i_right and data[i_left] <= data[i_mid]:
+        while i_left <= i_right and data[i_left] < data[i_mid]:
             i_left += 1
-        while i_right >= i_left and data[i_right] >= data[i_mid]:
+        while i_right >= i_left and data[i_right] > data[i_mid]:
             i_right -= 1
         if i_right >= i_left:
             tmp = data[i_left]
@@ -32,8 +32,7 @@ def split(data, i_left: int, i_right: int):
 
 
 if __name__ == "__main__":
-    data = [5,2,9,4,0,3,1]
-    #data = [5,2]
-    print(data)
-    quick_sort(data, 0, len(data)-1)
-    print(data)
+    unsorted = [5,2,9,4,0,3,1]
+    print(unsorted)
+    quick_sort(unsorted, 0, len(unsorted)-1)
+    print(unsorted)
